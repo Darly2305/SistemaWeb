a83521274af74c5e29daebdbbe654f8789944ccb
Darly <darly.juarez.2003@gmail.com>
2025-01-28 13:59:39 -0600

    se cambia docker

--- SistemaWeb/src/main/java/com/example/SistemaWeb/SistemaWebApplication.java
+++ SistemaWeb/src/main/java/com/example/SistemaWeb/SistemaWebApplication.java
@@ -2,6 +2,9 @@ package com.example.SistemaWeb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SistemaWebApplication {
@@ -9,5 +12,20 @@ public class SistemaWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaWebApplication.class, args);
 	}
+        
+        //Para realizar la configuración del CORS
+    @Bean
+    public WebMvcConfigurer corsConfigurer2() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowCredentials(true)
+                        .allowedHeaders("Origin", "Authorization", "Content-Type")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
 }
